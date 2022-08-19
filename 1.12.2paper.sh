7d1aa791e3701c51861d643e138ce7d38f56441a
Nebula <40174727+fgasdasda@users.noreply.github.com>
2022-08-19 23:29:00 +0200

    Create 1.12.2paper.sh
    Please Don't Report Any Bugs I Don't Really Know How To Fix It

---
+++ 1.12.2paper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update
+apt install software-properties-common
+add-apt-repository ppa:openjdk-r/ppa
+apt-get install openjdk-8-jre nano zip unzip
+mkdir mc
+cd mc
+wget -O minecraft_server.jar https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar 
+echo "eula=true" > eula.txt
+cd ..
+wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.zip --change if 32bit phone or if outdated
+unzip ngrok
+printf "cd mc\njava -Xmx4G -jar minecraft_server.jar nogui" > m   --change 4G if want lower or higher amounts of memory
+chmod +x m
